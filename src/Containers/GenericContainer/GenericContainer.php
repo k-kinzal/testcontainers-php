937e732f4d9d7073b350968f391a750d0ccf888d
k-kinzal <keen.flag7803@logn.in>
2025-02-11 13:30:49 +0900

    feat(GenericContainer): add StartupSetting trait for managing container startup timeout and check strategy

--- src/Containers/GenericContainer/GenericContainer.php
+++ src/Containers/GenericContainer/GenericContainer.php
@@ -42,6 +42,7 @@ class GenericContainer implements Container
     use NetworkModeSetting;
     use PrivilegeSetting;
     use PullPolicySetting;
+    use StartupSetting;
     use VolumesFromSetting;
     use WorkdirSetting;
 
@@ -74,36 +75,6 @@ class GenericContainer implements Container
      * @var string[]
      */
     private $commands = [];
-
-    /**
-     * Define the default startup timeout to be used for the container.
-     * @var int|null
-     */
-    protected static $STARTUP_TIMEOUT;
-
-    /**
-     * The startup timeout to be used for the container.
-     * @var int|null
-     */
-    private $startupTimeout;
-
-    /**
-     * Define the default startup check strategy to be used for the container.
-     * @var string|null
-     */
-    protected static $STARTUP_CHECK_STRATEGY;
-
-    /**
-     * The startup check strategy to be used for the container.
-     * @var StartupCheckStrategy|null
-     */
-    private $startupCheckStrategy;
-
-    /**
-     * The startup check strategy provider.
-     * @var StartupCheckStrategyProvider
-     */
-    private $startupCheckStrategyProvider;
 
     /**
      * Define the default port strategy to be used for the container.
@@ -203,26 +174,6 @@ class GenericContainer implements Container
     /**
      * {@inheritdoc}
      */
-    public function withStartupTimeout($timeout)
-    {
-        $this->startupTimeout = $timeout;
-
-        return $this;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function withStartupCheckStrategy($strategy)
-    {
-        $this->startupCheckStrategy = $strategy;
-
-        return $this;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
     public function withPortStrategy($strategy)
     {
         $this->portStrategy = $strategy;
@@ -256,43 +207,6 @@ class GenericContainer implements Container
         }
         if ($this->commands) {
             return $this->commands;
-        }
-        return null;
-    }
-
-    /**
-     * Retrieve the startup timeout for the container.
-     *
-     * @return int|null
-     */
-    protected function startupTimeout()
-    {
-        if (static::$STARTUP_TIMEOUT) {
-            return static::$STARTUP_TIMEOUT;
-        }
-        return $this->startupTimeout;
-    }
-
-    /**
-     * Retrieve the startup check strategy for the container.
-     *
-     * This method returns the startup check strategy that should be used for the container.
-     * If a specific startup check strategy is set, it will return that. Otherwise, it will
-     * attempt to retrieve the default startup check strategy from the provider.
-     *
-     * @return StartupCheckStrategy|null The startup check strategy to be used, or null if none is set.
-     */
-    protected function startupCheckStrategy()
-    {
-        if (static::$STARTUP_CHECK_STRATEGY !== null) {
-            $strategy = $this->startupCheckStrategyProvider->get(static::$STARTUP_CHECK_STRATEGY);
-            if (!$strategy) {
-                throw new LogicException("Startup check strategy not found: " . static::$STARTUP_CHECK_STRATEGY);
-            }
-            return $strategy;
-        }
-        if ($this->startupCheckStrategy) {
-            return $this->startupCheckStrategy;
         }
         return null;
     }
---
+++ tests/Unit/Containers/GenericContainer/StartupSettingTest.php
@@ -0,0 +1,72 @@
+<?php
+
+/** @noinspection PhpUnhandledExceptionInspection */
+
+namespace Tests\Unit\Containers\GenericContainer;
+
+use PHPUnit\Framework\TestCase;
+use RuntimeException;
+use Symfony\Component\Process\Exception\ProcessTimedOutException;
+use Testcontainers\Containers\GenericContainer\GenericContainer;
+use Testcontainers\Containers\GenericContainer\StartupSetting;
+use Testcontainers\Containers\StartupCheckStrategy\IsRunningStartupCheckStrategy;
+
+class StartupSettingTest extends TestCase
+{
+    public function testHasStartupSettingTrait()
+    {
+        $uses = class_uses(GenericContainer::class);
+
+        $this->assertContains(StartupSetting::class, $uses);
+    }
+
+    public function testStaticStartupTimeout()
+    {
+        $this->expectException(ProcessTimedOutException::class);
+
+        $container = (new StartupSettingWithStaticStartupTimeoutContainer('alpine:latest'))
+            ->withCommands(['sleep', '5']);
+        $container->start();
+    }
+
+    public function testStaticStartupCheckStrategy()
+    {
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Illegal state of container');
+
+        $container = (new StartupSettingWithStaticStartupCheckStrategyContainer('alpine:latest'))
+            ->withCommands(['echo', '1']);
+        $container->start();
+    }
+
+    public function testStartWithStartupTimeout()
+    {
+        $this->expectException(ProcessTimedOutException::class);
+
+        $container = (new GenericContainer('alpine:latest'))
+            ->withStartupTimeout(1)
+            ->withCommands(['sleep', '5']);
+        $container->start();
+    }
+
+    public function testStartWithStartupCheckStrategy()
+    {
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Illegal state of container');
+
+        $container = (new GenericContainer('alpine:latest'))
+            ->withStartupCheckStrategy(new IsRunningStartupCheckStrategy())
+            ->withCommands(['echo', '1']);
+        $container->start();
+    }
+}
+
+class StartupSettingWithStaticStartupTimeoutContainer extends GenericContainer
+{
+    protected static $STARTUP_TIMEOUT = 1;
+}
+
+class StartupSettingWithStaticStartupCheckStrategyContainer extends GenericContainer
+{
+    protected static $STARTUP_CHECK_STRATEGY = 'is_running';
+}
