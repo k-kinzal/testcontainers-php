dd46a4ee4f449c3a7b86f62df57ee5e4953366bb
k-kinzal <keen.flag7803@logn.in>
2025-03-05 22:30:29 +0900

    feat(Docker): add BuildCommand trait and DockerBuildOutput class

---
+++ src/Docker/Output/DockerBuildOutput.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Testcontainers\Docker\Output;
+
+/**
+ * Represents the output of a Docker `build` command executed via Symfony Process.
+ *
+ * This class extends DockerOutput to provide methods for retrieving the standard output,
+ * error output, and exit code of the Docker `build` command.
+ */
+class DockerBuildOutput extends DockerOutput
+{
+}
