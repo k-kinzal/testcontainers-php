d2b97db020bd7174cbb9e92c25642507facf9fcb
k-kinzal <keen.flag7803@logn.in>
2025-05-04 20:40:40 +0900

    Add PHPDoc for AlreadyExistsPortStrategyException constructor parameters

--- src/Containers/PortStrategy/AlreadyExistsPortStrategyException.php
+++ src/Containers/PortStrategy/AlreadyExistsPortStrategyException.php
@@ -4,8 +4,16 @@ namespace Testcontainers\Containers\PortStrategy;
 
 use Exception;
 
+/**
+ * An exception thrown when a port strategy with the same name already exists.
+ */
 class AlreadyExistsPortStrategyException extends Exception
 {
+    /**
+     * @param string $name The name of the port strategy that already exists.
+     * @param int $code The exception code.
+     * @param Exception|null $previous The previous exception.
+     */
     public function __construct($name, $code = 0, $previous = null)
     {
         parent::__construct('Port strategy with name ' . $name . ' already exists.', $code, $previous);
