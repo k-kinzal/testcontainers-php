8bfc4762b272639dabc422d6cdc6fa6a5d937932
k-kinzal <keen.flag7803@logn.in>
2025-05-04 20:20:01 +0900

    Add documentation for HttpProbe interface and available method

--- src/Containers/WaitStrategy/HttpProbeGetHeaders.php
+++ src/Containers/WaitStrategy/HttpProbeGetHeaders.php
@@ -2,8 +2,17 @@
 
 namespace Testcontainers\Containers\WaitStrategy;
 
+/**
+ * HttpProbeGetHeaders checks the availability of a specific HTTP endpoint by retrieving its headers.
+ *
+ * This class implements the HttpProbe interface and provides a method to check if the specified
+ * HTTP endpoint is available by checking its response headers.
+ */
 class HttpProbeGetHeaders implements HttpProbe
 {
+    /**
+     * {@inheritDoc}
+     */
     public function available($endpoint, $responseCode = 200)
     {
         $headers = @get_headers($endpoint);
@@ -11,6 +20,9 @@ class HttpProbeGetHeaders implements HttpProbe
             return false;
         }
         if (!isset($headers[0])) {
+            return false;
+        }
+        if (!is_string($headers[0])) {
             return false;
         }
         $statusCode = (int) substr($headers[0], 9, 3);
