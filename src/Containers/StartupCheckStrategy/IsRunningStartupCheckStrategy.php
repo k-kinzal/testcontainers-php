17f663ddddf2fc54181c5c81de74b29ee8b9c016
k-kinzal <keen.flag7803@logn.in>
2025-05-05 20:35:34 +0900

    Add timeout and retry interval configuration to IsRunningStartupCheckStrategy

--- src/Containers/StartupCheckStrategy/IsRunningStartupCheckStrategy.php
+++ src/Containers/StartupCheckStrategy/IsRunningStartupCheckStrategy.php
@@ -22,6 +22,20 @@ class IsRunningStartupCheckStrategy implements StartupCheckStrategy
     private $client;
 
     /**
+     * The timeout duration in seconds.
+     *
+     * @var int the timeout duration in seconds
+     */
+    private $timeout = 30;
+
+    /**
+     * The interval in microseconds to wait before retrying the check.
+     *
+     * @var int the interval in seconds
+     */
+    private $retryInterval = 0;
+
+    /**
      * Sets the docker client.
      *
      * @param DockerClient $client the docker client
@@ -36,6 +50,34 @@ class IsRunningStartupCheckStrategy implements StartupCheckStrategy
     }
 
     /**
+     * Sets the timeout duration for waiting until the container instance is ready.
+     *
+     * @param int $seconds the number of seconds to wait before timing out
+     *
+     * @return $this the current instance for method chaining
+     */
+    public function withTimeoutSeconds($seconds)
+    {
+        $this->timeout = $seconds;
+
+        return $this;
+    }
+
+    /**
+     * Sets the interval in microseconds to wait before retrying the check.
+     *
+     * @param int $interval the interval in microseconds
+     *
+     * @return $this the current instance for method chaining
+     */
+    public function withRetryInterval($interval)
+    {
+        $this->retryInterval = $interval;
+
+        return $this;
+    }
+
+    /**
      * Wait until the container startup is successful.
      *
      * @param ContainerInstance $instance the container instance to check
@@ -44,11 +86,17 @@ class IsRunningStartupCheckStrategy implements StartupCheckStrategy
      */
     public function waitUntilStartupSuccessful($instance)
     {
+        $now = time();
+
         $client = $this->client ?: DockerClientFactory::create();
 
         try {
             $this->logger()->debug('Waiting for container to be running...');
             while (true) {
+                if (time() - $now > $this->timeout) {
+                    throw new WaitingTimeoutException($this->timeout);
+                }
+
                 $output = $client->inspect($instance->getContainerId());
 
                 switch ($output->state->status) {
@@ -63,7 +111,7 @@ class IsRunningStartupCheckStrategy implements StartupCheckStrategy
                         $this->logger()->debug('Container is not running yet. Current status: ' . $output->state->status);
                         break;
                 }
-                usleep(0);
+                usleep($this->retryInterval);
             }
         } catch (\Exception $e) {
             $this->logger()->debug('Error while checking container status: ' . $e->getMessage());
